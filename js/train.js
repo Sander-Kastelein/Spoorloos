3e483aa01a1e2e7cc5f306d59864082f43720745
Reinder <shev22ppal@live.nl>
2016-10-26 14:36:33 +0200

    Train.json

--- js/train.js
+++ js/train.js
@@ -1,13 +1,37 @@
 "use strict";
 
-class Train extends THREE.Group
+class Train extends THREE.Mesh
 { 
 
-	constructor()
+	constructor(geometry)
 	{
-		
-		super(); // Invoke new THREE.Group()
+		super(geometry); // Invoke new THREE.Mesh()
+		game.scene.add(this);
+
 	}
 
+	update(delta)
+	{
+
+
+	}
 
 }
+
+
+function loadTrain(callback)
+{
+	var loader = new THREE.JSONLoader();
+	loader.load('models/3d-model.json', function(geometry) {
+		callback(new Train(geometry));
+	});
+}
+
+
+loadTrain(function(train){
+
+	train.position.x = 0;
+	train.position.y = 0;
+	trains.position.z = 0;
+
+});
