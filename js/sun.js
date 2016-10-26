e3899e99eb9b561c44126ceef4ab408ec613b037
Sander Kastelein <sander@sanderkastelein.nl>
2016-10-26 14:56:26 +0200

    Added documentation for colour shifting sun

--- js/sun.js
+++ js/sun.js
@@ -5,12 +5,24 @@ class Sun extends THREE.AmbientLight
 
 	constructor()
 	{
+		/*
+			00:00 : 0
+			01:00 : 3600
+			02:00 : 7200
+			12:00 : 43200
+			23:59 : 86340
+			24:00 : 0
+			
+		*/
+		this.timeColors = [
+			
+		];
+
+
 		super(0x404040, 5);
 		game.scene.add(this);
 		
-		this.position.y = 10;
-		this.position.x = 0;
-		this.position.z = 0;
+		
 
 	}
 
