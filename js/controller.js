2a5be3be4a5667542f282ae149590b07aafeba43
Sander Kastelein <sander@sanderkastelein.nl>
2016-10-27 12:36:00 +0200

    Fix OrbitControl dep

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,21 @@
 
 
-class Controller extends THREE.OrbitControls
+class Controller
 {
 
 
     constructor()
     {
-        super(game.camera, window);
-        this.enableDamping = true;
-        this.dampingFactor = 0.25;
-        this.enableZoom = true;
+        // super(game.camera, window);
+        // this.enableDamping = true;
+        // this.dampingFactor = 0.25;
+        // this.enableZoom = true;
     }
 
 
     update(delta)
     {
-    	super.update();
+    	// super.update();
     }
 
 
