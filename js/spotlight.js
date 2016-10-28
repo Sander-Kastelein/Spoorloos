d5cfe93bc34ed081437c70bcb5d3d89b66937733
RCAHaertlein <rca.haertlein@gmail.com>
2016-10-28 12:58:26 +0200

    added rotating sun

--- js/spotlight.js
+++ js/spotlight.js
@@ -7,17 +7,21 @@ class Spotlight extends THREE.SpotLight
     {
         let color = 0xffffff;
         let intensity = 5;
-        let distance = 1000;
+        let distance = 1200;
 
         super(color, intensity, distance);
         this.castShadow = true;
-        this.position.set(0,100,400);
+        this.position.set(0,1000,100);
+        this.angle = 0;
 
         game.scene.add(this);
     }
 
-    update()
+    update(delta)
     {
-
+        this.angle += delta;
+        this.position.x = 60 * Math.sin(this.angle);
+        this.position.z = 100 * Math.cos(this.angle);
+        this.position.y = 10 + 10 * Math.cos(this.angle/2);
     }
 }
