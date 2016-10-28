da315ecfe67c23c2bc6e6f0913290ba0dd00b252
Ben Blanco <Ben Blanco>
2016-10-28 13:21:10 +0200

    added texture to the restaurant

--- js/asset-loader.js
+++ js/asset-loader.js
@@ -48,6 +48,7 @@ assetLoader = new (class{
 		this.assets.trainTextureArriva = new THREE.ImageUtils.loadTexture("img/arriva.png");
 		this.assets.groundTexture = new THREE.ImageUtils.loadTexture("img/ground.jpg")
 		this.assets.trackTexture = new THREE.ImageUtils.loadTexture("img/track.jpg");
+		this.assets.restaurantTexture = new THREE.ImageUtils.loadTexture("img/smullers.jpg");
 
 	}
 
