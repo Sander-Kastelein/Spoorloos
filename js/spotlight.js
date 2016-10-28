f75053cfe088d002068ca207627b2892edbf5c3b
RCAHaertlein <rca.haertlein@gmail.com>
2016-10-28 13:18:50 +0200

    stopped sun rotation, added texture smullers

--- js/spotlight.js
+++ js/spotlight.js
@@ -6,22 +6,22 @@ class Spotlight extends THREE.SpotLight
     constructor()
     {
         let color = 0xffffff;
-        let intensity = 5;
-        let distance = 1200;
+        let intensity = 2;
+        let distance = 2000;
 
         super(color, intensity, distance);
         this.castShadow = true;
-        this.position.set(0,1000,100);
-        this.angle = 0;
+        this.position.set(0,200,700);
+        //this.angle = 0;
 
         game.scene.add(this);
     }
 
     update(delta)
     {
-        this.angle += delta;
-        this.position.x = 60 * Math.sin(this.angle);
-        this.position.z = 100 * Math.cos(this.angle);
-        this.position.y = 10 + 10 * Math.cos(this.angle/2);
+        //this.angle += 0.5 * delta;
+        //this.position.x = 10 * Math.sin(this.angle);
+        //this.position.z = 10 * Math.cos(this.angle);
+        //this.position.y = 500 * Math.cos(this.angle/2);
     }
 }
