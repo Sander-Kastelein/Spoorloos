42998ccc27f63955beabb2ee0d52ecfe0c8947fb
RCAHaertlein <rca.haertlein@gmail.com>
2016-10-31 14:10:37 +0100

    added skydome

--- js/game.js
+++ js/game.js
@@ -31,6 +31,7 @@ class Game
 		this.track = new Track();
         this.restaurant = new Restaurant();
 		this.hokje = new Hokje();
+		this.skydome = new SkyDome();
 
 	}
 
@@ -61,6 +62,7 @@ class Game
 		this.restaurant.update(delta);
 		this.hokje.update(delta);
 		this.hemisphere.update(delta);
+		this.skydome.update(delta);
 	}
 
 	start()
---
+++ js/skydome.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Ruud on 31-10-2016.
+ */
+class SkyDome extends THREE.Mesh
+{
+    constructor()
+    {
+        let vertexShader = document.getElementById( 'vertexShader' ).textContent;
+        let fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
+        let uniforms = {
+            topColor:    { value: new THREE.Color( 0x0077ff ) },
+            bottomColor: { value: new THREE.Color( 0xffffff ) },
+            offset:      { value: 33 },
+            exponent:    { value: 0.6 }
+        };
+
+        let skyGeo = new THREE.SphereGeometry( 4000, 32, 15 );
+        let skyMat = new THREE.ShaderMaterial( { vertexShader: vertexShader, fragmentShader: fragmentShader, uniforms: uniforms, side: THREE.BackSide } );
+
+        super(skyGeo, skyMat);
+            game.scene.add(this);
+    }
+
+    update()
+    {
+
+    }
+}
