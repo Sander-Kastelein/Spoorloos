401f4351911cb8a59132ff1980beb9971212427c
RCAHaertlein <rca.haertlein@gmail.com>
2016-11-07 11:05:39 +0100

    removed shit in sun

--- js/sun.js
+++ js/sun.js
@@ -43,10 +43,6 @@ class Sun extends THREE.Object3D
         {
             this.updatePosition();
         }
-
-            //this.position.x = 1000 * (Math.sin(deg2rad(getCurrentSunAngleInDegrees())));
-            //this.position.z = 1000 * (Math.cos(deg2rad(getCurrentSunAngleInDegrees())));
-            //this.position.y = 600 * (Math.sin(deg2rad(getCurrentSunAngleInDegrees())));
     }
 
     updatePosition()
