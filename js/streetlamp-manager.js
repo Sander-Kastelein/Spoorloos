b1219fdc187a7651ce278051b2b2bdf2f4dde0f4
RCAHaertlein <rca.haertlein@gmail.com>
2016-11-08 12:07:51 +0100

    changed lights positions

--- js/streetlamp-manager.js
+++ js/streetlamp-manager.js
@@ -13,8 +13,6 @@ class StreetlampManager
             (new Streetlamp( new THREE.Vector3(23.5,-4, 90), new THREE.Vector3(0, 360/360*Math.PI, 0))),
             (new Streetlamp( new THREE.Vector3(23.5,-4, 120), new THREE.Vector3(0, 360/360*Math.PI, 0))),
             (new Streetlamp( new THREE.Vector3(23.5,-4, 150), new THREE.Vector3(0, 360/360*Math.PI, 0))),
-            (new Streetlamp( new THREE.Vector3(23.5,-4, 180), new THREE.Vector3(0, 360/360*Math.PI, 0))),
-            (new Streetlamp( new THREE.Vector3(23.5,-4, 205), new THREE.Vector3(0, 360/360*Math.PI, 0))),
 
             //platform 1
             (new Streetlamp( new THREE.Vector3(-22.5,-4, 90))),
@@ -26,6 +24,9 @@ class StreetlampManager
             //platform 5/8
             (new Streetlamp( new THREE.Vector3(22,-4, -70), new THREE.Vector3(0, 360/360*Math.PI, 0),new THREE.Vector3(0.4, 1, 0.8))),
             (new Streetlamp( new THREE.Vector3(18.5,-4, -155), new THREE.Vector3(0, 360/360*Math.PI, 0),new THREE.Vector3(0.4, 1, 0.8))),
+
+            (new Streetlamp( new THREE.Vector3(4.3,-4, 102.5), new THREE.Vector3(0, 360/360*Math.PI, 0))),
+            (new Streetlamp( new THREE.Vector3(4.3,-4, 157.5), new THREE.Vector3(0, 360/360*Math.PI, 0))),
         ];
     }
 
